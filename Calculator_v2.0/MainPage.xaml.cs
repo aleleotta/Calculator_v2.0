a6195b61796c2207812692ad3d88c7841f2e5338
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-12 14:19:25 +0100

    Add project files.

---
+++ Calculator_v2.0/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator_v2._0
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void operations() { }
+        public void numbers() { }
+        public void display() { }
+
+    }
+}
