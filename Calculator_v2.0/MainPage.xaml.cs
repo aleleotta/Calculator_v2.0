5b6c0f95af728f55465682d8df86242cb020d630
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-13 19:54:55 +0100

    .

--- Calculator_v2.0/MainPage.xaml.cs
+++ Calculator_v2.0/MainPage.xaml.cs
@@ -8,8 +8,17 @@
             InitializeComponent();
         }
 
-        public void operations(object sender, EventArgs e) { }
+        public void operations(object sender, EventArgs e) {
+            string buttonName = add_button.Id.ToString();
+            switch (buttonName)
+            {
+                case "add_button":
+                    break;
+            }
+        }
+
         public void numbers(object sender, EventArgs e) { }
+
         public void display(object sender, EventArgs e) { }
 
     }
