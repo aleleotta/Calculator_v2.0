09c5e0c505446545751cef00bfcd50abaa60b3a2
aleleotta <114069815+aleleotta@users.noreply.github.com>
2024-03-14 10:18:14 +0100

    .

--- Calculator_v2.0/MainPage.xaml.cs
+++ Calculator_v2.0/MainPage.xaml.cs
@@ -3,23 +3,61 @@
     public partial class MainPage : ContentPage
     {
 
+        double result;
+        double num1;
+        double num2;
+        string selectedOperation;
+        bool twoNumbersSelected;
+
         public MainPage()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Evento asociado al click de botones de operacion y que asigna el valor a la variable selectedOperation
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void operations(object sender, EventArgs e) {
-            /*string buttonName = add_button.Id.ToString();
-            switch (buttonName)
+            Button buttonName = (Button)sender;
+            switch (buttonName.Text)
             {
-                case "add_button":
+                case "+":
+                    result = result + num1;
                     break;
-            }*/
+                case "-":
+                    result = result - num1;
+                    break;
+                case "*":
+                    result = result * num1;
+                    break;
+                case "/":
+                    result = result / num1;
+                    break;
+                case "=":
+                    break;
+                case "C":
+                    result = 0;
+                    num1 = 0;
+                    num2 = 0;
+                    calc_display.Text = "0";
+                    break;
+            }
         }
 
-        private void numbers(object sender, EventArgs e) { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numbers(object sender, EventArgs e) {
+            Button buttonName = (Button)sender;
+            //double.TryParse(num, out string numTxt);
+            
+        }
 
-        private void display(object sender, EventArgs e) { }
+        private void cancelOperation() { }
 
     }
 }
